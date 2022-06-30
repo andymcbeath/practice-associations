f638a3ede19c634a39cacb0f70a921b7b1978f6f
andymcbeath <mcbeath1@gmail.com>
2022-06-30 08:56:28 -0500

    Adds today's homework

---
+++ temporary_homework/questions_june30.rb
@@ -0,0 +1,11 @@
+# What is a strength of Rails (or something you like about Rails)?
+# It is pretty cool to be able to launch a server and then upload information to the web.
+
+# What is a "controller"? How is it different from a "route"?
+# A "controller" is what interacts with the program to produce a requested view or to alter a model. A route lives in the config/routes.rb. The Rails router recognises URLs and dispatches them to a controller's action. In addtion it can generate paths and URLs.
+
+# Why could it be useful to render json over html?
+# Because json is designed specifically for sharing data, making it faster. It is the format choice for almost every publicicly available web service.
+
+# List at least one question you have about Rails.
+# I don't have any questions at this time.
